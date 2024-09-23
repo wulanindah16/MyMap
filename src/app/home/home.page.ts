3af24ce1b16b16de59f18a17d0d153e5d5ebd749
wulanindah16 <wulanidh168@gmail.com>
2024-09-23 18:28:21 +0700

    Perubahan marker dan modifikasi tampilan web

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,15 +13,15 @@ export class HomePage {
   constructor() {}
 
   ionViewDidEnter() {
-    // Inisialisasi peta di elemen dengan ID "mapId"
-    this.map = L.map('mapId').setView([51.505, -0.09], 13);
+    // Inisialisasi peta
+    this.map = L.map('mapId').setView([51.502, -0.15], 11);
     
-    // Tambahkan layer peta
+    // Menambahkan layer peta
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    // Definisikan basemap
+    // Basemap
     const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
@@ -34,29 +34,48 @@ export class HomePage {
       attribution: '&copy; <a href="https://carto.com/attribution">CARTO</a>'
     });
 
+    const lightGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
+    });
+
+    const darkGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
+    });
+
     const esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
     });
 
     const esriWorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    const googleSatellite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      attribution: '&copy; <a href="https://www.google.com/intl/en_US/help/terms_maps/">Google</a>'
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
     });
 
-    // Tambahkan layer control untuk menampilkan opsi basemap
+    const esriOceans = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
+    });
+
+    const esriStreets = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
+    });
+
+    const esriNationalGeographic = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
+    });
+
+    // Layer control basemap
     const baseMaps = {
       'OpenStreetMap': openStreetMap,
       'Satellite Map': satelliteMap,
-      'Dark Map': darkMap,
+      'Dark Gray Canvas': darkGrayCanvas,
+      'Light Gray Canvas': lightGrayCanvas,
       'Satellite': esriWorldImagery,
       'Topographic': esriWorldTopoMap,
-      'Google Satellite': googleSatellite
+      'Oceans': esriOceans,
+      'Streets': esriStreets,
+      'National Geographic': esriNationalGeographic
     };
 
-    // Set OpenStreetMap sebagai layer default
+    // Mengatur default basemap
     openStreetMap.addTo(this.map);
 
     // Menambahkan layer control ke peta
@@ -64,15 +83,27 @@ export class HomePage {
 
     // Simbolisasi icon
     const customIcon = L.icon({
-      iconUrl: 'assets/icon/pin.png', 
+      iconUrl: 'assets/icon/park.png', 
       iconSize: [25, 25]
     });
 
-    // Tambahkan marker dengan popup
-    const marker1 = L.marker([51.505, -0.09], { icon: customIcon }).addTo(this.map);
-    marker1.bindPopup('Marker 1: Hello London!').openPopup();
+    // Marker Point dan Popup
+    const markerData: { coords: L.LatLngTuple, popup: string }[] = [
+      { coords: [51.5039, -0.143], popup: '<h5 style="text-align: center;">Green Park</h5><img src="assets/images/Green-Park.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' }, 
+      { coords: [51.4794, -0.156], popup: '<h5 style="text-align: center;">Battersea Park</h5><img src="assets/images/Battersea-Park.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.4846, -0.162], popup: '<h5 style="text-align: center;">Chelsea Physic Garden</h5><img src="assets/images/Chelsea-Physic-Garden.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.5313, -0.156], popup: '<h5 style="text-align: center;">Regents Park</h5><img src="assets/images/Regents-Park-.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.5076, -0.178], popup: '<h5 style="text-align: center;">Kensington Gardens</h5><img src="assets/images/Kensington-Gardens-.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.5075, -0.164], popup: '<h5 style="text-align: center;">Hyde Park</h5><img src="assets/images/hyde-park.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.5611, -0.163], popup: '<h5 style="text-align: center;">Hampstead Head</h5><img src="assets/images/Hampstead-Heath.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.4415, -0.274], popup: '<h5 style="text-align: center;">Richmond Park</h5><img src="assets/images/richmond.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.4772, 0.0016], popup: '<h5 style="text-align: center;">Greenwich Park</h5><img src="assets/images/greenwich.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' },
+      { coords: [51.5032, -0.134], popup: '<h5 style="text-align: center;">St James Park</h5><img src="assets/images/St-James-Park.jpg" width="150" height="100" style="display: block; margin: 0 auto;" />' }
+    ];
 
-    const marker2 = L.marker([51.515, -0.1], { icon: customIcon }).addTo(this.map);
-    marker2.bindPopup('Marker 2: Hello London!');
+    markerData.forEach((data, index) => {
+      const marker = L.marker(data.coords, { icon: customIcon }).addTo(this.map);
+      marker.bindPopup(data.popup);
+    });
   }
 }
